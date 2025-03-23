50124cc387c430e4c857705701041a6a9018881d
vanesixx <batyukvaness@gmail.com>
2025-03-23 20:21:35 +0100

    commit_org

---
+++ storage2.py
@@ -0,0 +1,54 @@
+products = [
+    {
+        "name": "Audi",
+        "price": 50
+    },
+    {
+        "price": 30,
+        "name": "BMW",
+    }
+]
+
+
+def print_products():
+    for product in products:
+        print(f"Název produktu2: {product['name']}, cena: {product['price']}$")
+
+
+def add_product():
+    product_name = input("Název produktu:")
+    product_price = input("Název cenu:")
+    product2 = {
+        'name': product_name,
+        'price': product_price
+    }
+
+    products.append(product2)
+
+
+def menu():
+    print("Vítej ve skladu")
+    print("###############\n")
+    print("1. Výpis polože")
+    print("2. Přidání položky\n")
+
+    choice = int(input("Volba: "))
+
+    if choice == 1:
+        print("Položky na skladě jsou:")
+        print_products()
+        print("")
+        menu()
+
+    elif choice == 2:
+        print("Přidání poožky:")
+        add_product()
+        print("")
+        menu()
+
+    else:
+        print("Zadal jsi špatně!\n")
+        menu()
+
+
+menu()
